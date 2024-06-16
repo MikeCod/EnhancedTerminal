87495557bd1f687895f89901ba9df4bf946095e5
Mike <mike.cod013@gmail.com>
2024-06-16 15:22:43 +0100

    fix: android studio

--- setup/android-studio.sh
+++ setup/android-studio.sh
@@ -6,6 +6,9 @@ version="2024.1.1.11"
 cd /opt
 wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/${version}/android-studio-${version}-linux.tar.gz
 tar xzvf ./android-studio-${version}-linux.tar.gz
+rm -rf ./android-studio-${version}-linux.tar.gz
+
+bash /opt/android-studio/bin/studio.sh
 
 echo 'export ANDROID_HOME=$HOME/Android/Sdk' >> ~/.zshrc
 echo 'export PATH=$PATH:$ANDROID_HOME/emulator' >> ~/.zshrc
