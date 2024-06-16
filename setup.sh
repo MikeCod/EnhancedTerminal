14a2918638aa935eeab67ccb3a8da8b67e2a5cef
Dreamer <dreamer@kali>
2024-06-16 15:10:44 +0100

    fix: setup

--- setup.sh
+++ setup.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
-# apt update -y
-# apt install -y \
-# 	highlight \
-# 	vim \
-# 	dialog \
-# 	snapd \
-# 	bluez \
-# 	bluez-tools \
-# 	blueman
+if [ "$EUID" -ne 0 ];then
+	echo 'Please run as root'
+	exit 1
+fi
 
-# dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
 
-# cp -v .zshrc ~/
-# cp -v vimrc /etc/vim/
+apt update -y
+apt install -y \
+	highlight \
+	vim \
+	dialog \
+	snapd \
+	bluez \
+	bluez-tools \
+	blueman
 
-# systemctl enable snapd && systemctl start snapd
-# systemctl enable bluetooth && systemctl start bluetooth
+dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
 
-function in_array() {
-	if [[ ${1[*]} =~ (^|[[:space:]])"$2"($|[[:space:]]) ]]; then
-		return "$3 "
-	else
-		return ""
+cp -v .zshrc ~/
+cp -v vimrc /etc/vim/
+
+systemctl enable snapd && systemctl start snapd
+systemctl enable bluetooth && systemctl start bluetooth
+
+
+containsElement () { for e in "${@:2}"; do [[ "$e" = "$1" ]] && return 0; done; return 1; }
+
+in_array() {
+	if [[ $(containsElement $1 ${@:3}) -eq 0 ]]; then
+		echo "$2 "
 	fi
 }
 
-function in_array_exec() {
-	if [[ ${1[*]} =~ (^|[[:space:]])"$2"($|[[:space:]]) ]]; then
-		/bin/bash setup/$3.sh
-		return "$4 "
-	else
-		return ""
+in_array_exec() {
+	if [[ $(containsElement $1 ${@:4}) -eq 0 ]]; then
+		/bin/bash setup/$2.sh
+		echo "$3 "
 	fi
 }
 
@@ -44,12 +49,11 @@ misc=$(dialog --checklist 'What do you want to install ?' 40 70 70 \
 	1 'PDF tools' on \
 	2 'GIMP' on \
 	3 'qBittorrent' on \
-	4 'Proton softwares (Beta Mail & VPN)' on 2>&1 >/dev/tty)
-
-aptn+=$(in_array $misc 1 "pdfid pdf-parser")
-aptn+=$(in_array $misc 2 "gimp")
-aptn+=$(in_array_exec $misc 3 "qbittorrent" "qbittorrent")
-aptn+=$(in_array_exec $misc 4 "proton" "proton-vpn-gnome-desktop")
+	4 'Proton softwares (Beta Mail & VPN)' off 2>&1 >/dev/tty)
+aptn+="$(in_array 1 'pdfid pdf-parser' $misc)"
+aptn+="$(in_array 2 'gimp' $misc)"
+aptn+="$(in_array_exec 3 'qbittorrent' 'qbittorrent' $misc)"
+aptn+="$(in_array_exec 4 'proton' 'proton-vpn-gnome-desktop' $misc)"
 
 
 dev=$(dialog --checklist 'Development:' 40 70 70 \
@@ -58,9 +62,9 @@ dev=$(dialog --checklist 'Development:' 40 70 70 \
 	12	'	Android Studio' off \
 	20 'Programming' off \
 	21	'	Node.js' on 2>&1 >/dev/tty)
-aptn+=$(in_array_exec $misc 11 "vscode" "code")
-aptn+=$(in_array_exec $misc 12 "android-studio")
-aptn+=$(in_array $dev 21 "nodejs")
+aptn+="$(in_array_exec 11 'vscode' 'code' $dev)"
+aptn+="$(in_array_exec 12 'android-studio' '' $dev)"
+aptn+="$(in_array 21 'nodejs' $dev)"
 
 entr=$(dialog --checklist 'Social and entertainment:' 40 40 60 \
 	10 'Social' on \
@@ -70,11 +74,11 @@ entr=$(dialog --checklist 'Social and entertainment:' 40 40 60 \
 	20 'Entertainment' on \
 	21	'	Spotify' on \
 	22	'	VLC' on 2>&1 >/dev/tty)
-snapn+=$(in_array $entr 11 "discord")
-snapn+=$(in_array $entr 12 "whatsdesk")
-snapn+=$(in_array $entr 13 "telegram-desktop")
-snapn+=$(in_array $entr 21 "spotify")
-snapn+=$(in_array $entr 22 "vlc")
+snapn+="$(in_array 11 'discord' $entr)"
+snapn+="$(in_array 12 'whatsdesk' $entr)"
+snapn+="$(in_array 13 'telegram-desktop' $entr)"
+snapn+="$(in_array 21 'spotify' $entr)"
+snapn+="$(in_array 22 'vlc' $entr)"
 
 exec 3>&-  # Close the temporary stream
 
