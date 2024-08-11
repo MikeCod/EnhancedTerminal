c48d75aa3d570bfbeadc2f3f4e7b338ae84b009d
Nightmare <night@eternal.home>
2024-08-11 19:53:37 +0200

    q

--- import.sh
+++ import.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-	echo 'Please run as root'
-	exit 1
-fi
-
-if [[ "$1" == "" ]]; then
-	echo 'Please specify a user'
+if [ "$EUID" -eq 0 ]; then
+	echo 'Please do NOT run as root'
 	exit 1
 fi
 
@@ -14,17 +9,11 @@ SCRIPTPATH="$(
 	cd -- "$(dirname "$0")" >/dev/null 2>&1
 	pwd -P
 )"
+
 conf="$SCRIPTPATH/conf/"
-userpath="/home/$1"
 
-if [ ! -d "$userpath" ]; then
-	echo "This user does not seem to exist."
-	echo "Checked: $userpath"
-	exit 1
-fi
-
-apt update
-apt install -y \
+sudo apt update
+sudo apt install -y \
 	highlight \
 	vim \
 	dialog \
@@ -33,35 +22,38 @@ apt install -y \
 	bluez-tools \
 	blueman \
 	hexedit \
-	code \
 	pdfid pdf-parser \
 	gimp \
 	libreoffice libreoffice-gnome \
 	qbittorrent \
 	vlc
 
-runuser - "$1" -c 'cd /tmp && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash && nvm install 20 && node -v && npm -v'
+cd /tmp && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash && nvm install 20 && node -v && npm -v
 
 cd $conf
 
 # Terminal
-dconf load /org/gnome/terminal/legacy/profiles:/ <gnome-terminal-profiles.dconf
-cp -v .zshrc "$userpath" && cp -v .zshrc /root/
-cp -v vimrc /etc/vim/
+dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
+cp -v .zshrc ~/ && sudo cp -v .zshrc /root/
+sudo cp -v vimrc /etc/vim/
 
 # Snap package manager
-systemctl enable snapd && systemctl start snapd && systemctl enable --now snapd.apparmor
+sudo systemctl enable --now snapd && sudo systemctl enable --now snapd.apparmor
 # Bluetooth
-systemctl enable bluetooth && systemctl start bluetooth
+sudo systemctl enable --now blueman-mechanism
 
 # App settings
 unpack() {
-	tar xJvf "$1.tar.xz" --directory "$2"
+	if [[ $3 == "true" ]]; then
+		sudo tar xJvf "$1.tar.xz" --directory "$2"
+	else
+		tar xJvf "$1.tar.xz" --directory "$2"
+	fi
 }
 
-unpack vscode "$userpath/.config/Code/User"
-unpack libreoffice "$userpath/.config/libreoffice/4/user"
-unpack fonts /usr/local/share/fonts
+unpack vscode "~/.config/Code/User/"
+unpack libreoffice "~/.config/libreoffice/4/user/"
+unpack fonts "/usr/local/share/fonts/" true
 
 # Gnome Settings
 ## Volume
@@ -79,12 +71,14 @@ gsettings set org.gnome.shell.keybindings show-screen-recording-ui "['<Shift><Co
 gsettings set org.gnome.desktop.sound event-sounds false
 gsettings set org.gnome.desktop.sound input-feedback-sounds false
 
-snap install \
+# https://github.com/fthx/dock-from-dash
+
+sudo snap install \
 	core \
 	discord \
 	whatsdesk \
 	telegram-desktop \
 	spotify
 
-cp -v ./whatsdesk.desktop /opt/
-desktop-file-install /opt/whatsdesk.desktop
+sudo cp -v ./whatsdesk.desktop /opt/
+sudo desktop-file-install /opt/whatsdesk.desktop
